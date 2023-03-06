31d9bcfe6af456ba1bd90d50fcc6500e45985b3d
AnnaStepanian <anna_stepanyan@edu.aua.am>
2023-03-06 22:07:06 +0400

    Improved the input fields by restricting them to accept only numbers within the range of 0 to 100. Additionally, divided lengthy functions into smaller, more manageable parts and organized them into separate classes.

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -3,169 +3,88 @@ package com.example.hw3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
-import android.text.InputType
 import android.widget.*
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var container1: LinearLayout
-    private lateinit var addButton: Button
-    private lateinit var container2: LinearLayout
-    private lateinit var addButton2: Button
-    private lateinit var participationInputText: EditText
-    private lateinit var groupPresentationInputText: EditText
-    private lateinit var finalProjectInputText: EditText
-    private lateinit var calculate: Button
-    private lateinit var reset: Button
-    private lateinit var result: TextView
-
-    private val MAX_CHILD_VIEWS = 5
+    private var homeworkManager: HomeworkManager? = null
+    private var midtermManager: MidtermManager? = null
+    private var homeworksGrade: LinearLayout? = null
+    private var addHwButton: Button? = null
+    private var midterms: LinearLayout? = null
+    private var addMidtermsButton: Button? = null
+    private var participationGrade: EditText? = null
+    private var groupPresentationGrade: EditText? = null
+    private var finalProjectGrade: EditText? = null
+    private var calculateButton: Button? = null
+    private var resetButton: Button? = null
+    private var overallGrade: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        container1 = findViewById(R.id.container1)
-        addButton = findViewById(R.id.add_button)
-        container2 = findViewById(R.id.container2)
-        addButton2 = findViewById(R.id.add_button2)
-        participationInputText = findViewById<EditText>(R.id.participation_grade)
-        groupPresentationInputText = findViewById<EditText>(R.id.group_project_grade)
-        finalProjectInputText = findViewById<EditText>(R.id.final_project_grade)
-        calculate = findViewById(R.id.calculate)
-        result = findViewById<TextView>(R.id.result_text_view)
-        reset = findViewById(R.id.reset)
+        homeworksGrade = findViewById(R.id.hw_grade)
+        addHwButton = findViewById(R.id.addHwButton)
+        midterms = findViewById(R.id.midterms_grade)
+        addMidtermsButton = findViewById(R.id.addMidtermButton)
+        participationGrade = findViewById<EditText>(R.id.participation_grade)
+        groupPresentationGrade = findViewById<EditText>(R.id.group_project_grade)
+        finalProjectGrade = findViewById<EditText>(R.id.final_project_grade)
+        calculateButton = findViewById(R.id.calculate)
+        overallGrade = findViewById<TextView>(R.id.result_text_view)
+        resetButton = findViewById(R.id.reset)
+        homeworkManager = HomeworkManager(this, homeworksGrade!!, addHwButton!!)
+        midtermManager = MidtermManager(this, midterms!!, addMidtermsButton!!)
 
-        participationInputText.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
-        groupPresentationInputText.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
-        finalProjectInputText.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
+        participationGrade!!.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
+        groupPresentationGrade!!.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
+        finalProjectGrade!!.filters = arrayOf<InputFilter>(InputFilterMinMax("0", "100"))
 
 
-
-
-        addButton.setOnClickListener {
-            if (container1.childCount < MAX_CHILD_VIEWS) {
-                val linearLayout = LinearLayout(this)
-                linearLayout.orientation = LinearLayout.HORIZONTAL
-
-                val editText = EditText(this)
-                editText.hint = "Homework grade ${container1.childCount + 1}"
-                editText.layoutParams = LinearLayout.LayoutParams(
-                    0,
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    1f
-                )
-                editText.inputType = InputType.TYPE_CLASS_NUMBER
-                editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(3),
-                    InputFilterMinMax("0", "100"))
-
-                val deleteButton = Button(this)
-                deleteButton.text = "Delete"
-                deleteButton.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                deleteButton.setOnClickListener {
-                    container1.removeView(linearLayout)
-                    addButton.isEnabled = true
-                    for (i in 0 until container1.childCount) {
-                        val child = container1.getChildAt(i) as LinearLayout
-                        val childEditText = child.getChildAt(0) as EditText
-                        childEditText.hint = "Homework grade ${i + 1}"
-                    }
-                }
-
-                linearLayout.addView(editText)
-                linearLayout.addView(deleteButton)
-
-                container1.addView(linearLayout)
-
-                if (container1.childCount == MAX_CHILD_VIEWS) {
-                    addButton.isEnabled = false
-                }
-            } else {
-                Toast.makeText(this, "Maximum limit reached", Toast.LENGTH_SHORT).show()
-            }
+        addHwButton!!.setOnClickListener {
+            homeworkManager!!.addHomework()
         }
 
 
-        addButton2.setOnClickListener {
-            if (container2.childCount < 2) {
-                val linearLayout = LinearLayout(this)
-                linearLayout.orientation = LinearLayout.HORIZONTAL
-
-                val editText = EditText(this)
-                editText.hint = "Midterm grade ${container2.childCount + 1}"
-                editText.layoutParams = LinearLayout.LayoutParams(
-                    0,
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    1f
-                )
-                editText.inputType = InputType.TYPE_CLASS_NUMBER
-                editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(3),
-                    InputFilterMinMax("0", "100"))
-
-                val deleteButton = Button(this)
-                deleteButton.text = "Delete"
-                deleteButton.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                deleteButton.setOnClickListener {
-                    container2.removeView(linearLayout)
-                    // Re-enable the add button
-                    addButton2.isEnabled = true
-
-                    for (i in 0 until container2.childCount) {
-                        val view = container2.getChildAt(i)
-                        if (view is LinearLayout) {
-                            val editText = view.getChildAt(0) as EditText
-                            editText.hint = "Midterm grade ${i + 1}"
-                        }
-                    }
-                }
-
-                linearLayout.addView(editText)
-                linearLayout.addView(deleteButton)
-
-                container2.addView(linearLayout)
-
-                if (container2.childCount == 2) {
-                    addButton2.isEnabled = false
-                }
-            } else {
-                Toast.makeText(this, "Maximum limit reached", Toast.LENGTH_SHORT).show()
-            }
+        addMidtermsButton!!.setOnClickListener {
+            midtermManager!!.addMidtermGrade()
         }
 
-        calculate.setOnClickListener {
-            val hwGrades = mutableListOf<Double>()
-            for (i in 1..5) {
-                val hwEditText = container1.findViewWithTag<TextInputEditText>("Homework $i")
-                if (!hwEditText?.text.isNullOrEmpty()) {
-                    hwGrades.add(hwEditText.text.toString().toDouble())
-                }
-            }
-            val midtermGrades = mutableListOf<Double>()
-            for (i in 1..2) {
-                val midtermEditText = container2.findViewWithTag<TextInputEditText>("Midterm $i")
-                if (!midtermEditText?.text.isNullOrEmpty()) {
-                    midtermGrades.add(midtermEditText.text.toString().toDouble())
-                }
-            }
-            val participationGrade = participationInputText.text.toString().toDoubleOrNull() ?: 0.0
-            val groupPresentationGrade = groupPresentationInputText.text.toString().toDoubleOrNull() ?: 0.0
-            val finalProjectGrade = finalProjectInputText.text.toString().toDoubleOrNull() ?: 0.0
 
-            val totalGrade = calculateTotal(participationGrade, hwGrades, groupPresentationGrade, midtermGrades, finalProjectGrade)
-            result.text = String.format("%.2f", totalGrade)
+        calculateButton!!.setOnClickListener {
+            calculateTotalGrade()
         }
-        reset.setOnClickListener {
+
+        resetButton!!.setOnClickListener {
             resetFields()
         }
 
     }
+    fun calculateTotalGrade() {
+        val hwGrades = mutableListOf<Double>()
+        for (i in 1..5) {
+            val hwEditText = homeworksGrade?.findViewWithTag<TextInputEditText>("Homework $i")
+            if (!hwEditText?.text.isNullOrEmpty()) {
+                hwGrades.add(hwEditText!!.text.toString().toDouble())
+            }
+        }
+        val midtermGrades = mutableListOf<Double>()
+        for (i in 1..2) {
+            val midtermEditText = midterms?.findViewWithTag<TextInputEditText>("Midterm $i")
+            if (!midtermEditText?.text.isNullOrEmpty()) {
+                midtermGrades.add(midtermEditText!!.text.toString().toDouble())
+            }
+        }
+        val participationGrade = participationGrade?.text.toString().toDoubleOrNull() ?: 0.0
+        val groupPresentationGrade = groupPresentationGrade?.text.toString().toDoubleOrNull() ?: 0.0
+        val finalProjectGrade = finalProjectGrade?.text.toString().toDoubleOrNull() ?: 0.0
+
+        val totalGrade = calculateTotal(participationGrade, hwGrades, groupPresentationGrade, midtermGrades, finalProjectGrade)
+        overallGrade?.text = String.format("%.2f", totalGrade)
+    }
+
+
     private fun calculateTotal(
         participationGrade: Double,
         hwGrades: List<Double>,
@@ -173,20 +92,24 @@ class MainActivity : AppCompatActivity() {
         midtermGrades: List<Double>,
         finalProjectGrade: Double
     ): Double {
-        val hwTotal = if (hwGrades.isNotEmpty()) hwGrades.average() * 100 * 0.2 else 0.0
-        val midtermTotal = if (midtermGrades.isNotEmpty()) midtermGrades.average() * 100 * 0.3 else 0.0
+        val hwTotal = if (hwGrades.isNotEmpty()) hwGrades.average() * 100 * 0.2 else 100.0
+        val firstMidtermGrade = if (midtermGrades.size > 0) midtermGrades[0] else 100.0
+        val secondMidtermGrade = if (midtermGrades.size > 1) midtermGrades[1] else 100.0
+        val midtermTotal = firstMidtermGrade * 0.1 + secondMidtermGrade * 0.2
         val totalGrade = participationGrade * 0.1 + hwTotal + groupPresentationGrade * 0.1 + midtermTotal + finalProjectGrade * 0.3
         return totalGrade
     }
+
     private fun resetFields() {
-        participationInputText.text.clear()
-        groupPresentationInputText.text.clear()
-        finalProjectInputText.text.clear()
-        container1.removeAllViews()
-        addButton.isEnabled = true
-        container2.removeAllViews()
-        addButton2.isEnabled = true
-        result.text = ""
+        participationGrade?.text?.clear()
+        groupPresentationGrade?.text?.clear()
+        finalProjectGrade?.text?.clear()
+        homeworksGrade?.removeAllViews()
+        addHwButton?.isEnabled = true
+        midterms?.removeAllViews()
+        addMidtermsButton?.isEnabled = true
+        overallGrade?.text = ""
     }
+
 
 }
---
+++ app/src/main/java/com/example/hw3/MidtermManager.kt
@@ -0,0 +1,101 @@
+package com.example.hw3
+
+import android.content.Context
+import android.text.InputFilter
+import android.text.InputType
+import android.text.Spanned
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+
+class MidtermManager(
+    private val context: Context,
+    private val midtermsLayout: LinearLayout,
+    private val addMidtermsButton: Button
+) {
+
+    companion object {
+        private const val MAX_MIDTERMS = 2
+    }
+
+    fun addMidtermGrade() {
+        if (midtermsLayout.childCount < MAX_MIDTERMS) {
+            val linearLayout = createLinearLayout()
+            val editText = createEditText(midtermsLayout.childCount + 1)
+            val deleteButton = createDeleteButton(linearLayout)
+
+            linearLayout.addView(editText)
+            linearLayout.addView(deleteButton)
+
+            midtermsLayout.addView(linearLayout)
+
+            if (midtermsLayout.childCount == MAX_MIDTERMS) {
+                addMidtermsButton.isEnabled = false
+            }
+        } else {
+            Toast.makeText(context, "Maximum limit reached", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun createLinearLayout(): LinearLayout {
+        val linearLayout = LinearLayout(context)
+        linearLayout.orientation = LinearLayout.HORIZONTAL
+        return linearLayout
+    }
+
+    private fun createEditText(index: Int): EditText {
+        val editText = EditText(context)
+        editText.hint = "Midterm grade $index"
+        editText.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            1f
+        )
+        editText.inputType = InputType.TYPE_CLASS_NUMBER
+        editText.filters = arrayOf<InputFilter>(
+            InputFilter.LengthFilter(3),
+            InputFilterMinMax("0", "100"),
+            object : InputFilter {
+                override fun filter(
+                    source: CharSequence?,
+                    start: Int,
+                    end: Int,
+                    dest: Spanned?,
+                    dstart: Int,
+                    dend: Int
+                ): CharSequence? {
+                    if (dest.toString() == "0" && source.toString() != "") {
+                        // prevent adding new digits when input is already 0
+                        return ""
+                    }
+                    return null
+                }
+            }
+        )
+        return editText
+    }
+
+    private fun createDeleteButton(linearLayout: LinearLayout): Button {
+        val deleteButton = Button(context)
+        deleteButton.text = "Delete"
+        deleteButton.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        deleteButton.setOnClickListener {
+            midtermsLayout.removeView(linearLayout)
+            addMidtermsButton.isEnabled = true
+            updateEditTextHints()
+        }
+        return deleteButton
+    }
+
+    private fun updateEditTextHints() {
+        for (i in 0 until midtermsLayout.childCount) {
+            val child = midtermsLayout.getChildAt(i) as LinearLayout
+            val childEditText = child.getChildAt(0) as EditText
+            childEditText.hint = "Midterm grade ${i + 1}"
+        }
+    }
+}
